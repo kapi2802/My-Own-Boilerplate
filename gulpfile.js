0a3eb7bdf762860831f3a6989a8e21ec7303b1d7
kapi2802 <kapi2802@gmail.com>
2016-11-14 16:42:15 +0100

    Add
    gulpfile.js and package.json

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+var sourceMaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('reload', function () {
+
+    browserSync.reload();
+
+});
+
+gulp.task('serve', ['sass'], function () {
+
+    browserSync({
+
+        server: 'src'
+
+    });
+
+    gulp.watch('src/*.html', ['reload']);
+    gulp.watch('src/scss/*.scss', ['sass']);
+
+});
+
+gulp.task('sass', function() {
+    
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sourceMaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            
+            browsers: ['last 5 versions']
+        
+        }))
+        .pipe(sourceMaps.write())
+        .pipe(gulp.dest('src/css'))
+        .pipe(browserSync.stream());
+    
+});
+
+gulp.task('default', ['serve']);
